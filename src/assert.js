b6f0f4f5c609658daaaf29f5a623f30ade652aad
Nicola Peduzzi <thenikso@gmail.com>
2021-01-22 11:50:12 +0100

    Prepare for pixelmatch

--- src/assert.js
+++ src/assert.js
@@ -1,4 +1,5 @@
 import { equal as eq } from './lib/utils';
+import pm from 'pixelmatch';
 import { AssertionError } from './errors';
 
 export const equal = (a, b) => {
@@ -6,4 +7,9 @@ export const equal = (a, b) => {
     // TODO better reppresent errors
     throw new AssertionError('a !== b');
   }
+};
+
+export const imageMatch = (options) => async (a, b) => {
+  // TODO a/b are canvas or img
+  // TODO opts is threshold
 };
