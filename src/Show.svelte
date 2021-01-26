ae3c0b22b17b2f1e993af7c0354c35a1ba983ab7
Nicola Peduzzi <thenikso@gmail.com>
2021-01-26 23:31:33 +0100

    Show has no wrapper by default

--- src/Show.svelte
+++ src/Show.svelte
@@ -1,21 +1,58 @@
 <script>
+  import { onDestroy } from 'svelte';
+
   export let element;
+  export let wrapped = false;
 
   let container;
+  const comment = document.createComment('show DOM element');
+
+  onDestroy(() => {
+    if (!container.parentElement) {
+      comment.parentElement.insertBefore(container, comment);
+      comment.remove();
+    }
+  });
 
   $: if (container) {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
+    if (wrapped && comment.parentElement) {
+      comment.parentElement.insertBefore(container, comment);
+      comment.remove();
+    } else if (container.parentElement) {
+      container.parentElement.insertBefore(comment, container);
+      container.remove();
     }
-    appendChilds(container, element);
   }
 
-  function appendChilds(el, chi) {
-    if (Array.isArray(chi)) {
-      chi.forEach((c) => appendChilds(el, c));
-    } else if (chi instanceof Node) {
-      el.appendChild(chi);
+  $: elements = (Array.isArray(element) ? element : [element])
+    .map((e) => {
+      if (typeof e === 'string') {
+        return document.createTextNode(e);
+      }
+      return e;
+    })
+    .filter((e) => e instanceof Node);
+
+  $: if (container) updateElements(elements);
+
+  let currentElements;
+
+  function updateElements(elements) {
+    if (currentElements) {
+      for (const c of currentElements) {
+        c.remove();
+      }
     }
+    if (wrapped) {
+      for (const e of elements) {
+        container.appendChild(e);
+      }
+    } else {
+      for (const e of elements) {
+        comment.parentElement.insertBefore(e, comment);
+      }
+    }
+    currentElements = elements.slice();
   }
 </script>
 
